d106e62ed145b08ccda45c32a8d9ab3c4a8a21ea
HaiderMalik12 <haidermalik504@gmail.com>
2023-08-02 17:11:00 +0500

    complete route with param

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {"message": "Hello world, I am learning FAST API"}
+
+
+@app.get("/items/{item_id}")
+def get_item_by_id(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/albums/{album_id}")
+def get_album(album_id: int):
+    return {"album_id": album_id}
