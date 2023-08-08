6475a22c7ee0905206cdbb395f7700ba74725107
HaiderMalik12 <haidermalik504@gmail.com>
2023-08-08 20:08:58 +0500

    adding response type

--- routers/tracks.py
+++ routers/tracks.py
@@ -6,7 +6,7 @@ tracks_router = APIRouter()
 tracks = []
 
 
-@tracks_router.get("/tracks")
+@tracks_router.get("/tracks", response_model=list[TrackModel])
 def get_tracks() -> dict:
     return {
         "tracks": tracks
@@ -14,18 +14,16 @@ def get_tracks() -> dict:
 
 
 @tracks_router.post("/tracks")
-def create_track(track: TrackModel) -> dict:
+def create_track(track: TrackModel) -> TrackModel:
     tracks.append(track)
-    return {
-        "tracks": tracks
-    }
+    return track
 
 
 @tracks_router.get("/tracks/{track_id}")
-def get_track_by_id(track_id: int) -> dict:
+def get_track_by_id(track_id: int) -> TrackModel:
     for track in tracks:
         if track.id == track_id:
-            return {"track": track}
+            return track
 
 
 @tracks_router.delete("/tracks/{track_id}")
@@ -38,11 +36,11 @@ def get_track_by_id(track_id: int) -> dict:
 
 
 @tracks_router.put("/tracks/{track_id}")
-def get_track_by_id(track_id: int, track_dto: TrackUpdateModel) -> dict:
+def get_track_by_id(track_id: int, track_dto: TrackUpdateModel) -> TrackModel:
     tracks_len_response = len(tracks)
     print(tracks_len_response)
     for index in range(tracks_len_response):
         track = tracks[index]
         if track.id == track_id:
             track.title = track_dto.title
-            return {"track": track}
+            return track
