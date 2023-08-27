b52d0e8e945b8d8b183b2facfa523f7690e04427
HaiderMalik12 <haidermalik504@gmail.com>
2023-08-27 18:04:20 +0500

    save user in db

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
-from .. import schemas
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from .. import schemas, dependencies, models
 
 users_router = APIRouter()
 
 # Singup new user
 
 
-@users_router.post("/users")
-def create_user(user_dto: schemas.UserCreate):
-    return 'Signup Route'
+@users_router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(dependencies.get_db)):
+    db_user = models.User(**user.model_dump())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
