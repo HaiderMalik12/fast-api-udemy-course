5000a58521866e860877799d52dcdf87dd740764
HaiderMalik12 <haidermalik504@gmail.com>
2023-08-27 17:39:21 +0500

    create user table

--- app/models.py
+++ app/models.py
@@ -8,3 +8,10 @@ class Track(Base):
     title = Column(String)
     released_date = Column(DATE)
     duration = Column(TIME)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
