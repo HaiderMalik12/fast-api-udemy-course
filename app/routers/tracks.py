c95c90249f1be2ae493b7b5533277a0c6a6895e5
HaiderMalik12 <haidermalik504@gmail.com>
2023-08-29 00:33:15 +0500

    completed router prefix

--- app/routers/tracks.py
+++ app/routers/tracks.py
@@ -7,31 +7,34 @@ from app.schemas import TrackModel, TrackUpdateModel, Track, TrackCreate, TrackU
 from app.dependencies import get_db
 from app.crud.tracks_crud import create_track, get_tracks, get_track_by_id, delete_track_by_id, update_track_by_id
 
-tracks_router = APIRouter()
+tracks_router = APIRouter(
+    prefix="/tracks",
+    tags=["Tracks"]
+)
 
 tracks = []
 
 
-@tracks_router.get("/tracks", response_model=List[Track])
+@tracks_router.get("/", response_model=List[Track])
 def get_tracks_route(db: Session = Depends(get_db)):
     return get_tracks(db=db)
 
 
-@tracks_router.post("/tracks", response_model=Track)
+@tracks_router.post("/", response_model=Track)
 def create_track_route(track_dto: TrackCreate, db: Session = Depends(get_db)):
     return create_track(db=db, track_dto=track_dto)
 
 
-@tracks_router.get("/tracks/{track_id}", response_model=Track)
+@tracks_router.get("/{track_id}", response_model=Track)
 def get_track_route(track_id: int, db: Session = Depends(get_db)):
     return get_track_by_id(db, track_id)
 
 
-@tracks_router.delete("/tracks/{track_id}")
+@tracks_router.delete("/{track_id}")
 def delete_track_route(track_id: int, db: Session = Depends(get_db)):
     return delete_track_by_id(db=db, track_id=track_id)
 
 
-@tracks_router.put("/tracks/{track_id}", response_model=Track)
+@tracks_router.put("/{track_id}", response_model=Track)
 def update_track_route(track_id: int, track_dto: TrackUpdate, db: Session = Depends(get_db)):
     return update_track_by_id(db=db, track_id=track_id, track_dto=track_dto)
--- app/routers/users.py
+++ app/routers/users.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from .. import schemas, dependencies, models, utils
 
-users_router = APIRouter()
+users_router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
 
 # Singup new user
 
 
-@users_router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
+@users_router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(dependencies.get_db)):
     hashed_password = utils.get_hashed_password(user.password)
     user.password = hashed_password
